b01870fcdaac5090b8c22307ad21cad6c51648f6
Elizaveta <elizaveta.shabanova-danielyan@learnit.fi>
2025-01-02 23:17:56 +0200

    values and variables

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+//Extra assignments:
+// Values and variables
+let country = "Finland";
+let continent = "Europe";
+let population = 5000000;
+console.log(country);
+console.log(continent);
+console.log(population);
