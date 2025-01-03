34b81c11ea55e50f51aa11910debe5c96e11ed18
Elizaveta <elizaveta.shabanova-danielyan@learnit.fi>
2025-01-03 13:45:04 +0200

    Data types and basic operators excersises

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -5,7 +5,31 @@ console.log(40 + 8 + 23 - 10);
 // Values and variables
 let country = "Finland";
 let continent = "Europe";
-let population = 5000000;
+let population = 6000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//Data types
+const isIsland = false;
+let language = "Finnish";
+console.log(isIsland);
+console.log(language);
+
+//Basic operators
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+population ++;
+console.log(population);
+population --;
+console.log(population == 6000000);
+if (population < 33000000) console.log('The country is less than an average country');
+let description = 
+    country + 
+    " is in " + 
+    continent + 
+    ", and it's " + 
+    population + 
+    " people speak " 
+    + language;
+console.log(description);
